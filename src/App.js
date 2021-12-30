f910d892416e6f0d862f18238b946adcfbe98677
bhawna-13 <bhawnamona3@gmail.com>
2021-12-30 10:14:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Cards from "./Extra Assests/Cards";
+import Footer from "./Extra Assests/Footer";
+import "./App.css";
 
 function App() {
+  const [isDateLoaded, setIsDateLoaded] = React.useState(false);
+  const [userData, setUserData] = React.useState([]);
+  const [isButtonClick, setisButtonClick] = React.useState(false);
+  const handleClick = () => {
+    setisButtonClick(true);
+    // To fetch data from https://reqres.in/api/users?page=1
+    fetch("https://reqres.in/api/users?page=1")
+      .then((response) => response.json())
+      .then((res) => {
+        setUserData(res.data);
+        setInterval(() => {
+          setIsDateLoaded(true);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <section className="wholecontainer">
+          <div class="topnav" id="myTopnav">
+            <a
+              rel="noreferrer"
+              href="https://srblgmvipt1.netlify.app"
+              target="_blank"
+              class="active"
+            >
+              ZIPPY
+            </a>
+            <a href="#getusers" class="btn" onClick={handleClick}>
+              Get Users
+            </a>
+          </div>
+
+          <div className="container">
+            <div className="row justify-content-center ">
+              {isButtonClick ? (
+                isDateLoaded ? (
+                  <Cards userData={userData} />
+                ) : (
+                  <div class="loader"></div>
+                )
+              ) : (
+                <div className="instruction">
+                  <p>To get the users of Zippy, click on "Get Users" button</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </section>
+        <section>
+          <Footer />
+        </section>
+      </Router>
+    </>
   );
 }
 
